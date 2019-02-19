f53fc51a9f6d1fa8090939972b4181e84b4e1f2f
UmeshKanase <ukanase@gmail.com>
2019-02-18 22:19:43 -0700

    Added singlton implementation.

---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance { get; set; }
+
+        private static object synchronizationObject = new object();
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetLogger()
+        {
+            if (_instance == null)
+            {
+                lock (synchronizationObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Design Patterns examples and explaination about when to use which pattern.
+
+Creational Design Pattern
+1. Singleton:
+	Difference between singleton and static class
+	Singleton can implement interface or be derived from base class
+	Instance of singleton can be passed as parameter from one method to another.
